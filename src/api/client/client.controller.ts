a1f157008c33754d77c054a9544ab81784d26922
benzispawn <spawnbenzi@gmail.com>
2023-03-03 00:21:59 -0300

    Adjusting client controller

--- src/api/client/client.controller.ts
+++ src/api/client/client.controller.ts
@@ -1,14 +1,25 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Inject, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Controller, Get, Inject, Param, ParseIntPipe, Post, Query } from "@nestjs/common";
 import { ClientService } from './client.service';
 import { Client } from '../../model';
-@Controller('client')
+@Controller('api/client')
 export class ClientController {
     @Inject(ClientService)
     private readonly service: ClientService;
 
+    @Get()
+    public getAll(@Query('name') query: string): Promise<Client[]> {
+        if (!!query) {
+            return this.service.getClientByName(query);
+        }
+        return this.service.getAll();
+    }
+
     @Get(':id')
-    public getClient(@Param('id', ParseIntPipe) id: number): Promise<Client> {
+    public getClient(@Param('id', ParseIntPipe) id: number, @Query('name') query: string): Promise<Client|Client[]> {
+        if (!!query) {
+            return this.service.getClientByIdWithBook(id, query);
+        }
         return this.service.getClient(id);
     }
 
--- src/api/client/client.service.ts
+++ src/api/client/client.service.ts
@@ -1,17 +1,35 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Client } from '../../model';
+import { ILike, Repository } from "typeorm";
+import { Books, Client } from "../../model";
 
 @Injectable()
 export class ClientService {
     @InjectRepository(Client)
     private readonly repository: Repository<Client>;
 
+    public getAll(): Promise<Client[]> {
+        return this.repository.find();
+    }
+
     public getClient(id: number): Promise<Client> {
         return this.repository.findOneBy({ id: id });
     }
+
+    public getClientByName(name: string): Promise<Client[]> {
+        return this.repository.findBy({
+            name: ILike(`%${name}%`)
+        })
+    }
+
+    public getClientByIdWithBook(id: number, name: string): Promise<Client[]|Client> {
+        return this.repository.createQueryBuilder('client')
+          .innerJoinAndMapMany('client.books', Books, 'bks', 'bks.cli_id = client.cli_id')
+          .where("client.cli_id = :id", { id: id })
+          .getMany()
+    }
+
     /**
      * public createUser(body: CreateUserDto): Promise<User> {
     const user: User = new User();
